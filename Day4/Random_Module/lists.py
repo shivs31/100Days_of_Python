0dd602bbff73f672ca5ddcf12812f66a8dc5061c
Shivani <rekala.shivani@gmail.com>
2024-09-10 16:50:33 +0200

    Lists

---
+++ Day4/Random_Module/lists.py
@@ -0,0 +1,20 @@
+states_of_america = ["Delaware", "Pennsylvania", "New Jersey", "Georgia", "Connecticut", "Massachusetts", "Maryland", "South Carolina", "New Hampshire", "Virginia", "New York", "North Carolina", "Rhode Island", "Vermont", "Kentucky", "Tennessee", "Ohio", "Louisiana", "Indiana", "Mississippi", "Illinois", "Alabama", "Maine", "Missouri", "Arkansas", "Michigan", "Florida", "Texas", "Iowa", "Wisconsin", "California", "Minnesota", "Oregon", "Kansas", "West Virginia", "Nevada", "Nebraska", "Colorado", "North Dakota", "South Dakota", "Montana", "Washington", "Idaho", "Wyoming", "Utah", "Oklahoma", "New Mexico", "Arizona", "Alaska", "Hawaii"]
+
+#print(states_of_america[1]
+
+#to alter/replace the data in the list
+states_of_america[1] = "Pencilvania"
+
+print(states_of_america[1])
+
+#add an item to the list at the end of the list
+
+states_of_america.append("Angelaland")
+
+print(states_of_america)
+
+#add bunch of items to the list
+
+states_of_america.extend(["Jack Bauer Land","New York"])
+
+print(states_of_america)
